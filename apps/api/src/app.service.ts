31cd64a3d72c921fbc04a918587a1657334ccbde
Gabriel Duarte <jds.gabrielduarte@gmail.com>
2023-09-18 15:50:08 -0300

    feat: online 2

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'NestJS Application';
+    return 'NestJS Application 1231';
   }
 }
